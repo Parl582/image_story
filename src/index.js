45462896d3c082fccdc037dda356c365f9de2fb6
Parl582 <pranjalgogoi582@gmail.com>
2024-04-10 23:00:43 +0530

    init

---
+++ src/index.js
@@ -0,0 +1,24 @@
+// import React from "react";
+// import ReactDOM from "react-dom/client";
+// import "./index.css";
+// import "react-image-crop/dist/ReactCrop.css";
+// // import CropImg from "./CropImg";
+// // import Upload from "./CropImg";
+
+// const root = ReactDOM.createRoot(document.getElementById("root"));
+// root.render(
+//   <React.StrictMode>
+//     <App />
+//     {/* <Upload /> */}
+//   </React.StrictMode>
+// );
+
+import React from "react";
+import ReactDOM from "react-dom";
+
+import App from "./App2";
+
+// import './styles.css';
+
+const rootElement = document.getElementById("root");
+ReactDOM.render(<App />, rootElement);
